ef4cce7004032bbb3edc1678eae13a0f1851377f
takishun <takishun.st3260@gmail.com>
2020-06-16 18:33:18 +0900

    status label num =>text

--- app/Task.php
+++ app/Task.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    // status
+    const STATUS = [
+      1 => ['label' => "未着手"],
+      2 => ['label' => "着手中"],
+      3 => ['label' => "完了"],
+    ];
+
+    public function getStatusLabelAttribute()
+    {
+        $status = $this->attributes['status'];
+
+        if(!isset(self::STATUS[$status])){
+          return '';
+        }
+
+        return self::STATUS[$status]['label'];
+    }
+
 }
