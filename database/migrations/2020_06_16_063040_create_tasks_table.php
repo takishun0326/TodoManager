385d0210580a1e01ba0151acc957141f74e408da
takishun <takishun.st3260@gmail.com>
2020-06-16 15:48:22 +0900

    tasks migrate

--- database/migrations/2020_06_16_063040_create_tasks_table.php
+++ database/migrations/2020_06_16_063040_create_tasks_table.php
@@ -14,8 +14,15 @@ class CreateTasksTable extends Migration
     public function up()
     {
         Schema::create('tasks', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->integer('folder_id')->unsigned();
+            $table->string('title', 100);
+            $table->date('due_data');
+            $table->integer('status')->default(1);
             $table->timestamps();
+
+            // 外部キー制約を設定
+            $table->foreign('folder_id')->references('id')->on('folders');
         });
     }
 
